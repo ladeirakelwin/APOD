544524f094ad015fa134eec1bbfd1331164753c9
ladeirakelwin <ladeirakelwin@gmail.com>
2020-07-04 11:56:32 -0300

    site do projeto apod

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
- 
+let oReq = new XMLHttpRequest();
+
+oReq.open('GET','https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw');
+oReq.onload = function() {
+  //mandar 'recebe' para uma funçaão
+  let recebe = JSON.parse(oReq.responseText);
+  console.log(recebe);
+
+  let data = document.querySelector(".data");
+  data.textContent = recebe.date.split("-").reverse().join("-");
+
+  let legenda = document.querySelector(".legenda");
+  legenda.textContent = recebe.title;
+
+  let url = document.querySelector(".url");
+  url.src = recebe.url;
+
+  let copyright = document.querySelector(".copyright");
+  copyright.textContent = "Image Credit & Copyright: "
+  + recebe.copyright;
+
+  let explanacao = document.querySelector(".explanacao");
+  explanacao.textContent = recebe.explanation;
+
+}
+oReq.send();
+
+
+
+
+
