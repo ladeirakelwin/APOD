d196214a3debef4f37429b6ca1ce0a410a64f3cc
ladeirakelwin <ladeirakelwin@gmail.com>
2020-07-05 19:23:44 -0300

    url da requisicao formatada

--- js/form.js
+++ js/form.js
@@ -1,7 +1,13 @@
+let urlDaApi = 'https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw'
+
 let form = document.querySelector('.form');
 let input = document.querySelector('#data')
 
 form.addEventListener('submit', function(event){
   event.preventDefault();
-  console.log(input.value);
+  let reqMedia = new XMLHttpRequest();
+  urlDaApi = urlDaApi+`&date=${input.value}`
+  // reqMedia.open('GET', urlData)
 });
+
+
