fddd3484ee9d24f32fb002498e1185767bd0d64f
ladeirakelwin <ladeirakelwin@gmail.com>
2020-07-05 19:29:26 -0300

    funcionalidade buscar media

--- js/form.js
+++ js/form.js
@@ -1,13 +1,22 @@
 let urlDaApi = 'https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw'
 
 let form = document.querySelector('.form');
-let input = document.querySelector('#data')
+let input = document.querySelector('#data');
+let body = document.querySelector('body');
 
 form.addEventListener('submit', function(event){
   event.preventDefault();
   let reqMedia = new XMLHttpRequest();
-  urlDaApi = urlDaApi+`&date=${input.value}`
-  // reqMedia.open('GET', urlData)
+  urlDaApi = urlDaApi+`&date=${input.value}`;
+  reqMedia.open('GET', urlDaApi);
+  reqMedia.onload = () => {
+    let req = JSON.parse(reqMedia.responseText);
+    let img = document.createElement('img');
+    img.src = req.url;
+    body.appendChild(img);    
+  }
+  
+  reqMedia.send();
 });
 
 
