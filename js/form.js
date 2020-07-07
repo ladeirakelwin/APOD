0579687d5dcf9445cf488180c86d769df96c0fdd
ladeirakelwin <ladeirakelwin@gmail.com>
2020-07-07 12:22:44 -0300

    projeto corrigido

--- js/form.js
+++ js/form.js
@@ -1,27 +1,4 @@
-let urlDaApi = 'https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw'
 
-let form = document.querySelector('.form');
-let input = document.querySelector('#data');
+
 let poteImg = document.querySelector('.poteImg');
 
-form.addEventListener('submit', function(event){
-  event.preventDefault();
-  let reqMedia = new XMLHttpRequest();
-
-  atualizaUrl(`&date=${input.value}`);
-  reqMedia.open('GET', urlDaApi);
-  reqMedia.onload = () => {
-    let req = JSON.parse(reqMedia.responseText);
-    let img = document.createElement('img');
-    img.src = req.hdurl;
-    img.classList.add('img');
-    poteImg.appendChild(img);  
-    console.log(urlDaApi)  
-  }
-  
-  reqMedia.send();
-});
-
-function atualizaUrl(url){
-  urlDaApi = urlDaApi + url;
-}
--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 let oReq = new XMLHttpRequest();
+let form = document.querySelector('.form');
+let input = document.querySelector('#data');
 
-oReq.open('GET','https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw');
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  oReq.open('GET', `https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw&date=${input.value}`);
+  oReq.send();
+});
+
+function dataFormatada(value){
+  return value.date.split("-").reverse().join("-");
+}
+
+oReq.open('GET',`https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw`);
 oReq.onload = function() {
   //mandar 'recebe' para uma funçaão
   let recebe = JSON.parse(oReq.responseText);
   console.log(recebe);
 
   let data = document.querySelector(".data");
-  data.textContent = recebe.date.split("-").reverse().join("-");
+  data.textContent = dataFormatada(recebe); 
 
   let legenda = document.querySelector(".legenda");
   legenda.textContent = recebe.title;
@@ -16,8 +28,12 @@ oReq.onload = function() {
   url.src = recebe.url;
 
   let copyright = document.querySelector(".copyright");
-  copyright.textContent = "Image Credit & Copyright: "
-  + recebe.copyright;
+  if(recebe.copyright != undefined){
+    copyright.textContent = "Image Credit & Copyright: "
+    + recebe.copyright;
+  }else{
+    copyright.textContent = "";
+  }
 
   let explanacao = document.querySelector(".explanacao");
   explanacao.textContent = recebe.explanation;
