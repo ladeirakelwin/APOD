debdd1acddbf80f8453d2e5c7cf4b8400bf0ae02
ladeirakelwin <ladeirakelwin@gmail.com>
2020-07-07 20:30:54 -0300

    filtro de img e video, e invisibilidade.

--- js/index.js
+++ js/index.js
@@ -4,15 +4,15 @@ let input = document.querySelector('#data');
 
 form.addEventListener('submit', function(event){
   event.preventDefault();
-  oReq.open('GET', `https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw&date=${input.value}`);
+  oReq.open('GET', `https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw&hd=true&date=${input.value}`);
   oReq.send();
 });
 
 function dataFormatada(value){
-  return value.date.split("-").reverse().join("-");
+  return value.date.split("-").reverse().join(" ");
 }
 
-oReq.open('GET',`https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw`);
+oReq.open('GET',`https://api.nasa.gov/planetary/apod?api_key=4aCgrQiswA81Mfaa3PslbnTq3goBgmcpaG4kTQSw&hd=true`);
 oReq.onload = function() {
   //mandar 'recebe' para uma funçaão
   let recebe = JSON.parse(oReq.responseText);
@@ -24,8 +24,18 @@ oReq.onload = function() {
   let legenda = document.querySelector(".legenda");
   legenda.textContent = recebe.title;
 
-  let url = document.querySelector(".url");
-  url.src = recebe.url;
+  let img = document.querySelector(".img");
+  let video = document.querySelector(".video");
+
+  if(recebe.media_type === "image"){
+    img.classList.remove("oculto");
+    video.classList.add("oculto");
+    img.src = recebe.url
+  }else{
+    video.classList.remove("oculto");
+    img.classList.add("oculto");
+    video.src = recebe.url;
+  }
 
   let copyright = document.querySelector(".copyright");
   if(recebe.copyright != undefined){
